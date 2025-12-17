ea7d6c7fb5b66362a9f6d943fd50142dee8a8ad8
gittus4ar <git1tus4ar@gmail.com>
2025-12-17 14:13:24 +0000

    Fix Flutter watcher error in CI-2

--- lib/features/auth/auth_service.dart
+++ lib/features/auth/auth_service.dart
@@ -3,14 +3,14 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AuthService {
-  // static final String _baseUrl = dotenv.env['API_BASE_URL']!;
+  static final String _baseUrl = dotenv.env['API_BASE_URL']!;
 
   static Future<Map<String, dynamic>> login({
     required String username,
     required String password,
   }) async {
-    final url = Uri.parse("http://192.168.1.9:5260/api/Auth/login");
-
+    final url = Uri.parse("$_baseUrl/api/pk/Customer/auth");
+    // API_BASE_URL=http://192.168.1.9:5260/api/pk/Customer/auth
     final response = await http.post(
       url,
       headers: {
