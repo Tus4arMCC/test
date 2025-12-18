16e980e7276552f1cc68a8f6d4417f2cf06fc313
gittus4ar <git1tus4ar@gmail.com>
2025-12-18 09:37:26 +0000

    register-screen-added test-2

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../features/auth/login_screen.dart';
 import '../features/main/main_tab_screen.dart';
 import '../features/main/profile_screen.dart';
+import '../features/auth/register_screen.dart';
+// import '../features/auth/forgot_password_screen.dart';
 
 class AppRoutes {
   static const login = '/';
@@ -12,5 +14,7 @@ class AppRoutes {
     login: (_) => const LoginScreen(),
     main: (_) => const MainTabScreen(),
     profile: (_) => const ProfileScreen(),
+    '/register': (_) => const RegisterScreen(),
+    // '/forgot': (_) => const ForgotPasswordScreen(),
   };
 }
