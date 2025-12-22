b4503fd545f65841caf0211f048a1c255137bba2
gittus4ar <git1tus4ar@gmail.com>
2025-12-22 11:15:41 +0000

    start home page-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'features/main/main_tab_screen.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/theme_controller.dart';
@@ -8,6 +9,7 @@ import 'app/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Hive.initFlutter();
   await CacheManager.instance.init();
   runApp(const MyApp());
